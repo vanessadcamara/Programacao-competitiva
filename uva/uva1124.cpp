e1e2a181e46324ab1b6d36d3684d3586f38328e2
vanessadcamara <vanessadcamara@gmail.com>
2020-12-22 20:39:37 -0400

    uva 1124

---
+++ uva/uva1124.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string s;
+    while(getline(cin, s)){
+        for (int i = 0; i < s.size(); i++)
+        {
+            cout << s[i];
+        }
+        cout << endl;
+    }
+    return 0;
+}

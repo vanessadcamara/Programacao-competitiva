25b775d6006907e4033d6753a4b1a796febad5e5
vanessadcamara <vanessadcamara@gmail.com>
2020-12-22 20:38:36 -0400

    uva272

--- uva/uva-272.cpp
+++ uva/uva-272.cpp
@@ -1,14 +1,22 @@
 #include<bits/stdc++.h>
 using namespace std;
-
 int main(){
-    vector<string> v;
     string s;
-    while(cin >> s){
-        if(s[i] == '"')
-        
-        v.push_back(s);
+    while(getline(cin,s)){
+        int cont = 0;
+        for(int i = 0; i < s.size(); i++){
+            if(s[i] == '"'){
+                if(cont == 0){
+                    cont = 1;
+                    cout << "``";
+                }else{
+                    cont = 0;
+                    cout << "''";
+                }
+            }
+            else 
+                cout << s[i];
+        }cout << "\n";
     }
-"""
     return 0;
 }

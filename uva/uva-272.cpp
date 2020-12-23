ea573372d49a1adb2f31d2fa4e0e1fc70f9449f4
vanessadcamara <vanessadcamara@gmail.com>
2020-12-22 20:37:04 -0400

    update

---
+++ uva/uva-272.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    vector<string> v;
+    string s;
+    while(cin >> s){
+        if(s[i] == '"')
+        
+        v.push_back(s);
+    }
+"""
+    return 0;
+}
